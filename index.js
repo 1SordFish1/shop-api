b12f17e3cf8bd13346e2e8770e728ab5ec2d8b8f
Achuothan M <achuothan.m@centizen.com>
2025-06-16 15:55:16 +0530

    cors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const port = 5000;
-const serverless = require('serverless-http');
+// const serverless = require('serverless-http');
 const env = process.env.ENVIRONMENT || 'dev';
 const allowedOrigins = ['http://localhost:4200'];
 
@@ -10,24 +10,24 @@ console.log('env..', env);
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'x-api-key']
-// }));
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'x-api-key']
+}));
 
 // Handle preflight
-// app.options('*', cors());
-app.use(express.json());
+app.options('*', cors());
+// app.use(express.json());
 // app.use('/data/images', express.static('data/images'));
-const s1 = require('./routes/s1');
-app.use('/s1', s1);
+// const s1 = require('./routes/s1');
+// app.use('/s1', s1);
 
 // if (env === 'prod') {
 //   // Serverless for Vercel
