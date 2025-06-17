7779e4f707178c34995eae9670f782c9fee41f9c
Achuothan M <achuothan.m@centizen.com>
2025-06-17 11:47:18 +0530

    undo deploy

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 5000;
-const appController = require('./controller/app.controller');
 const allowedOrigins = ['http://localhost:4200', 'https://shop-seven-amber.vercel.app/'];
 
 app.use(cors({
@@ -19,14 +18,10 @@ app.use(cors({
 
 // Handle preflight
 app.options('*', cors());
-// app.use(express.json());
-// app.use('/data/images', express.static('data/images'));
-// const s1 = require('./routes/s1');
-// app.use('/s1', s1);
-app.get("/", (req, res) => res.send("Express on Vercel"));
-app.get('/hello', appController.hello);
-app.get('/products', appController.getProducts);
-app.get('/products/:id', appController.getProductById);
+app.use(express.json());
+app.use('/data/images', express.static('data/images'));
+const s1 = require('./routes/s1');
+app.use('/s1', s1);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
