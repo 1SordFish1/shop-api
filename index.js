2b1725434506c4c6bd8148d19072de773cd2616d
Achuothan M <achuothan.m@centizen.com>
2025-06-17 11:41:24 +0530

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 5000;
+const appController = require('./controller/app.controller');
 const allowedOrigins = ['http://localhost:4200', 'https://shop-seven-amber.vercel.app/'];
 
 app.use(cors({
@@ -18,11 +19,17 @@ app.use(cors({
 
 // Handle preflight
 app.options('*', cors());
-app.use(express.json());
-app.use('/data/images', express.static('data/images'));
-const s1 = require('./routes/s1');
-app.use('/s1', s1);
+// app.use(express.json());
+// app.use('/data/images', express.static('data/images'));
+// const s1 = require('./routes/s1');
+// app.use('/s1', s1);
+
+app.get('/hello', appController.hello);
+app.get('/products', appController.getProducts);
+app.get('/products/:id', appController.getProductById);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+module.exports = app;
